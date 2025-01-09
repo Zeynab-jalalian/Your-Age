45b3110ae0e582bed07a79c68de9f8b10dbf8b11
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-09 19:54:54 +0330

    js 2

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let input=document.querySelector('#birthdate');
 let button=document.querySelector('#calculate');
+
+button.addEventListener('click',function(){
+   let birthDate=new Date(input.value);
+   console.log(birthDate);
+   
+})

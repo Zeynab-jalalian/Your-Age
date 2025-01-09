27677a35d0187ccff763101450c1c5d3bc502414
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-09 20:09:25 +0330

    js 4

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let input=document.querySelector('#birthdate');
 let button=document.querySelector('#calculate');
 
 let TodayDate=new Date();
+let TodayYear=TodayDate.getFullYear();
 button.addEventListener('click',function(){
    let birthDate=new Date(input.value);
+   let birthYear=birthDate.getFullYear();
+   let birthMonth=birthDate.getMonth();
+   let birthDay=birthDate.getDay();
+   let age=TodayYear-birthYear;
+   console.log(age);
+   
 })
